5b84754f0d5db61e2a0e73efd7864a670bbc28ab
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2016-07-19 18:21:04 +0200

    example: change license to GPLv2

--- examples/MAX1464_example.ino
+++ examples/MAX1464_example.ino
@@ -1,19 +1,19 @@
 /*
-  MAX1464 library for Arduino
+  Example sketch for the MAX1464 library.
   Copyright (C) 2016 Giacomo Mazzamuto <gmazzamuto@gmail.com>
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful,
+  This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library. If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "printhex.h"
